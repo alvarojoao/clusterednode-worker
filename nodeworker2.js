7bb3c538f2cee9ecf9f0714dfbb9a9dda78f5b98
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-06-18 16:33:07 -0300

    Code cleanup

--- nodeworker2.js
+++ nodeworker2.js
@@ -1,30 +1,27 @@
 require('pmx').init({
-	                    http:          true, // HTTP routes logging (default: true)
-	                    errors:        true, // Exceptions loggin (default: true)
-	                    custom_probes: true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
-	                    network:       true, // Network monitoring at the application level
-	                    ports:         false // Shows which ports your app is listening on (default: false)
-});
+                        http:          true, // HTTP routes logging (default: true)
+                        errors:        true, // Exceptions loggin (default: true)
+                        custom_probes: true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
+                        network:       true, // Network monitoring at the application level
+                        ports:         false // Shows which ports your app is listening on (default: false)
+                    });
 var http2     = require('http2'),
     onHeaders = require('on-headers'),
     fs        = require('fs'),
     os        = require('os'),
-    hostname  = os.hostname(),
+    options   = {
+        key:  fs.readFileSync('./nginx-selfsigned.key'),
+        cert: fs.readFileSync('./nginx-selfsigned.crt')
+    },
     nis       = os.networkInterfaces(),
-    eth       = nis[process.env.INTERFACE][0].address,
-    pid       = process.pid,
     offset    = parseFloat(process.env.TIMEOFFSET),
-    msg       = '{"hostname":"' + hostname + '","ipaddress":"' + eth + '","pid":' + pid + '}';
-var server = http2.createServer({
-                                    key: fs.readFileSync('./nginx-selfsigned.key'),
-                                    cert: fs.readFileSync('./nginx-selfsigned.crt')
-}, function(req, res) {
+    msg       = '{"hostname":"' + os.hostname() + '","ipaddress":"' + nis[process.env.INTERFACE][0].address + '","pid":' + process.pid + '}';
+var server = http2.createServer(options, function(req, res) {
     var startAt = process.hrtime();
     onHeaders(res, function onHeaders() {
-      var diff = process.hrtime(startAt);
-      var time = diff[0] * 1e3 + diff[1] * 1e-6 + offset;
-      var val = time.toFixed(3);
-      res.setHeader('X-Node-Time', val);
+        var diff = process.hrtime(startAt),
+            time = diff[0] * 1e3 + diff[1] * 1e-6 + offset;
+        res.setHeader('X-Node-Time', time.toFixed(3));
     });
     res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
     res.setHeader('Pragma', 'no-cache');
@@ -32,10 +29,10 @@ var server = http2.createServer({
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Pragma, Cache-Control, If-Modified-Since, X-ReqId");
     res.setHeader("Content-Type", "application/json");
-    res.setHeader("X-ReqId",req.headers['x-reqid']||"-1");
+    res.setHeader("X-ReqId", req.headers['x-reqid'] || "-1");
     res.end(msg);
 }).listen(8010);
-process.on('SIGINT', function(){
+process.on('SIGINT', function() {
     server.close();
-    setTimeout(function(){process.exit(0);}, 300);
+    setTimeout(function() { process.exit(0); }, 300);
 });
