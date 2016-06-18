1db3af3d933774430ae04880d1c4c5fda1c418e9
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-06-18 11:52:44 -0300

    Initial commit

---
+++ nodeworker2.js
@@ -0,0 +1,40 @@
+require('pmx').init({
+  http          : true, // HTTP routes logging (default: true)
+  errors        : true, // Exceptions loggin (default: true)
+  custom_probes : true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
+  network       : true, // Network monitoring at the application level
+  ports         : true  // Shows which ports your app is listening on (default: false)
+});
+var http2 = require('http2'),
+    onHeaders = require('on-headers'),
+    fs = require('fs'),
+    os = require('os'),
+    hostname = os.hostname(),
+    ni = os.networkInterfaces().eth0[0].address,
+    pid = process.pid,
+    offset = 0.9,
+    msg = '{"hostname":"'+hostname+'","ipaddress":"'+ni+'","pid":'+pid+'}';
+var server = http2.createServer({
+	key: fs.readFileSync('./nginx-selfsigned.key'),
+        cert: fs.readFileSync('./nginx-selfsigned.crt')
+}, function(req, res) {
+    var startAt = process.hrtime();
+    onHeaders(res, function onHeaders() {
+      var diff = process.hrtime(startAt)
+      var time = diff[0] * 1e3 + diff[1] * 1e-6 + offset;
+      var val = time.toFixed(3);
+      res.setHeader('X-Node-Time', val);
+    });
+    res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', 'Mon, 26 Jul 1997 05:00:00 GMT');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Pragma, Cache-Control, If-Modified-Since, X-ReqId");
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader("X-ReqId",req.headers['x-reqid']||"-1");
+    res.end(msg);
+}).listen(8010);
+process.on('SIGINT', function(){
+    server.close();
+    setTimeout(function(){process.exit(0);}, 300);
+});
