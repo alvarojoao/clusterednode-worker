53a7beb60edefbf8df9ca236239f2e7a0979b12b
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-07-03 19:50:48 -0300

    Simpler implementation - operation chosen by client or stress tool

--- nodeworker2.js
+++ nodeworker2.js
@@ -1,9 +1,9 @@
 'use strict';
 require('pmx').init({
-                        http:          true, // HTTP routes logging (default: true)
-                        errors:        true, // Exceptions loggin (default: true)
+                        http:          false, // HTTP routes logging (default: true)
+                        errors:        false, // Exceptions loggin (default: true)
                         custom_probes: true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
-                        network:       true, // Network monitoring at the application level
+                        network:       false, // Network monitoring at the application level
                         ports:         false // Shows which ports your app is listening on (default: false)
                     });
 var http2        = require('http2'),
