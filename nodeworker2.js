840f163f0090e2faf8446c3742b314e7185a79d4
Giancarlo Bonansea <giancarlobonansea@gbMBPro-2.local>
2016-08-05 10:01:53 -0300

    Back from ES6 to ES5 due to performance issues on Node.js 6.3.1

--- nodeworker2.js
+++ nodeworker2.js
@@ -1,3 +1,4 @@
+'use strict';
 require('pmx').init({
                         http:          true, // HTTP routes logging (default: true)
                         errors:        true, // Exceptions loggin (default: true)
@@ -5,16 +6,18 @@ require('pmx').init({
                         network:       true, // Network monitoring at the application level
                         ports:         false // Shows which ports your app is listening on (default: false)
                     });
-const http2 = require('http2'),
-    onHeaders = require('on-headers'),
-    fs = require('fs'),
-    url = require('url'),
-    os = require('os'),
-    Redis = require('ioredis'),
-    hostname    = os.hostname(),
+var http2         = require('http2'),
+    onHeaders     = require('on-headers'),
+    fs            = require('fs'),
+    tls           = require('tls'),
+    url           = require('url'),
+    os            = require('os'),
+    Redis         = require('ioredis'),
+    hostname      = os.hostname(),
     net           = os.networkInterfaces(),
     netIf         = (net.eth1 === undefined) ? '127.0.0.1' : net.eth1[0].address,
     pid           = process.pid,
+    redisReady    = false,
     rmOK          = 'OK',
     rmERROR       = 'ERR',
     raSET         = 'SET',
@@ -23,17 +26,16 @@ const http2 = require('http2'),
     raPIPELINE    = 'PPL',
     hdREDIS       = 'x-redis-time',
     hdNODE        = 'x-node-time';
-let redisReady    = false;
 // Defines certificates for enabling TLSv1.2
 //
-const sslCerts = {
+var sslCerts = {
     key:  fs.readFileSync('./nginx-selfsigned.key'),
     cert: fs.readFileSync('./nginx-selfsigned.crt')
 };
 //
 // Create redis cluster client
 //
-const cluster = new Redis.Cluster(
+var cluster = new Redis.Cluster(
     [
         {port: 6379, host: "127.0.0.1"},
         {port: 6378, host: "127.0.0.1"},
@@ -60,75 +62,75 @@ const cluster = new Redis.Cluster(
 //
 // Set redis events listeners
 //
-cluster.on("ready", () => {
+cluster.on("ready", function() {
     redisReady = true;
     console.log("redis.io cluster connections opened - ready to serve");
 });
-cluster.on("end", () => {
+cluster.on("end", function() {
     redisReady = false;
     console.log("redis.io cluster connections closed");
 });
-cluster.on("error", err => {
+cluster.on("error", function(err) {
     console.log("redis.io Error: " + err);
 });
-cluster.on("node error", err => {
+cluster.on("node error", function(err) {
     console.log("redis.io node Error: " + err);
 });
 //
 // Create diff hrtime header
 //
-function createDiffHrtimeHeader(headerLabel, startHRTime, httpResponse) {
-    const diffR = process.hrtime(startHRTime),
+var createDiffHrtimeHeader = function(headerLabel, startHRTime, httpResponse) {
+    var diffR = process.hrtime(startHRTime),
         timeR = diffR[0] * 1e3 + diffR[1] * 1e-6;
     httpResponse.setHeader(headerLabel, timeR.toFixed(3));
-}
+};
 //
 // Message handlers
 //
-function messageHandler(jsonMsg, httpResponse, redisAction, redisValue) {
+var messageHandler = function(jsonMsg, httpResponse, redisAction, redisValue) {
     jsonMsg.a = redisAction;
     jsonMsg.o = redisValue;
     httpResponse.end(JSON.stringify(jsonMsg));
-}
+};
 //
 // Send default ERR response due to a Redis error
 //
-function sendRedisError(jsonMsg, redisError, httpResponse, startHRTime) {
+var sendRedisError = function(jsonMsg, redisError, httpResponse, startHRTime) {
     createDiffHrtimeHeader(hdREDIS, startHRTime, httpResponse);
     messageHandler(jsonMsg, httpResponse, rmERROR, {});
-    //console.log(redisError);
-}
+    console.log(redisError);
+};
 //
 // Send composite message based on Redis results
 //
-function sendRedisResults(jsonMsg, httpResponse, redisAction, redisValue, startHRTime) {
+var sendRedisResults = function(jsonMsg, httpResponse, redisAction, redisValue, startHRTime) {
     createDiffHrtimeHeader(hdREDIS, startHRTime, httpResponse);
     messageHandler(jsonMsg, httpResponse, redisAction, redisValue);
-}
+};
 //
 // Create jsonObject
 //
-function jsonObject() {
+var jsonObject = function() {
     return {
         h: hostname,
         p: pid,
         t: Date.now()
     };
-}
+};
 //
 // Generic call wrapper
 //
-function genericCallWrapper(jM, hR, p, prCb, okCb) {
-    const src = process.hrtime();
+var genericCallWrapper = function(jM, hR, p, prCb, okCb) {
+    var src = process.hrtime();
     prCb(p).then(
         okCb(jM, hR, src),
-        rE => sendRedisError(jM, rE, hR, src)
+        function(rE) { sendRedisError(jM, rE, hR, src); }
     );
-}
+};
 //
 // Prepare multicommand pipeline (for PIPELINE and TRANSACTION)
 //
-function prepareCommands(p) {
+var prepareCommands = function(p) {
     return [
         ['hgetall',
          p],
@@ -136,68 +138,84 @@ function prepareCommands(p) {
          p,
          jsonObject()]
     ];
-}
+};
 //
 // Prepare execution function stack
 //
-const executionMatrix = [
+var executionMatrix = [
     //
     // HGETALL call
     //
     [
-        p => { return cluster.hgetall(p); },
-        (jM, hR, src) => {
-            return rM => sendRedisResults(jM, hR, raGET, (rM === '') ? {} : rM, src);
+        function(p) {
+            return cluster.hgetall(p);
+        },
+        function(jM, hR, src) {
+            return function(rM) {
+                sendRedisResults(jM, hR, raGET, (rM === '') ? {} : rM, src);
+            };
         }
     ],
     //
     // HMSET call
     //
     [
-        p => { return cluster.hmset(p, jsonObject()); },
-        (jM, hR, src) => {
-            return rM => sendRedisResults(jM, hR, (rM === rmOK) ? raSET : rmERROR, {}, src);
+        function(p) {
+            return cluster.hmset(p, jsonObject());
+        },
+        function(jM, hR, src) {
+            return function(rM) {
+                sendRedisResults(jM, hR, (rM === rmOK) ? raSET : rmERROR, {}, src);
+            };
         }
     ],
     //
     // PIPELINE call
     //
     [
-        p => { return cluster.pipeline(prepareCommands(p)).exec(); },
-        (jM, hR, src) => {
-            return rM => sendRedisResults(jM, hR, raPIPELINE, (rM.length === 0) ? {} : rM[0][1], src);
+        function(p) {
+            return cluster.pipeline(prepareCommands(p)).exec();
+        },
+        function(jM, hR, src) {
+            return function(rM) {
+                sendRedisResults(jM, hR, raPIPELINE, (rM.length === 0) ? {} : rM[0][1], src);
+            };
         }
     ],
     //
     // TRANSACTION call
     //
     [
-        p => { return cluster.multi(prepareCommands(p)).exec(); },
-        (jM, hR, src) => {
-            return rM => sendRedisResults(jM, hR, raTRANSACTION, (rM.length === 0) ? {} : rM[0][1], src);
+        function(p) {
+            return cluster.multi(prepareCommands(p)).exec();
+        },
+        function(jM, hR, src) {
+            return function(rM) {
+                sendRedisResults(jM, hR, raTRANSACTION, (rM.length === 0) ? {} : rM[0][1], src);
+            };
         }
     ]
 ];
 //
 // Set HTTP headers
 //
-function setAllHeaders(hRq, hR) {
+var setAllHeaders = function(hRq, hR) {
     hR.setHeader("Access-Control-Allow-Origin", "*");
     hR.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Pragma, Cache-Control, If-Modified-Since, x-reqid");
     hR.setHeader("Content-Type", "application/json");
-}
+};
 //
 // Main HTTPS server handler
 //
-const server = http2.createServer(sslCerts, (hRq, hR) => {
-    const startNodeCall = process.hrtime(),
+var server = http2.createServer(sslCerts, function(hRq, hR) {
+    var startNodeCall = process.hrtime(),
+        jM            = {
+            h: hostname,
+            p: pid
+        },
         params        = url.parse(hRq.url, true).query,
         o             = params.o || 0,
         p             = params.p || 0;
-    let jM            = {
-            h: hostname,
-            p: pid
-        };
     onHeaders(hR, function onHeaders () {
         createDiffHrtimeHeader(hdNODE, startNodeCall, hR);
     });
@@ -214,7 +232,7 @@ const server = http2.createServer(sslCerts, (hRq, hR) => {
 // Enables graceful stop/reload - nicely closes connections to redis and closes HTTPS server
 // enabling last transactions, both on redis and HTTPS server to be completed before exiting
 //
-process.on('SIGINT', () => {
+process.on('SIGINT', function() {
     //
     // finishes all redis transactions and closes connection with redis
     //
@@ -226,5 +244,5 @@ process.on('SIGINT', () => {
     //
     // nicely exit node after 0.3 seconds
     //
-    setTimeout(() => process.exit(0), 300);
+    setTimeout(function() { process.exit(0); }, 300);
 });
